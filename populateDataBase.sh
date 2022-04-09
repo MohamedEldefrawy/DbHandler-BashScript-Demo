5e8fbe2a963da9723918abfd7171770471db9316
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-04-09 15:31:00 +0200

    Finishing lab

---
+++ populateDataBase.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+############## Script parse text file saperated by spaces and insert it into database
+### Exit codes
+###             0: Success
+###             4: Missing parameter
+
+DB_USER="root"
+DB_PASSWD="Mohamed\$5265104@D"
+
+OrderInsert=$(awk '{print "insert into Bills.Orders values(" $1 ",\"" $2 "\", \"" $3 "\");"}' ./Orders.txt)
+OrderDetailsInsert=$(awk '{print "insert into Bills.OrderDetails values(" $1 "," $2 ", \"" $3 "\", "$4","$5");"}' ./OrderDetails.txt)
+
+mysql --user=$DB_USER --password=$DB_PASSWD -e "$OrderInsert"
+mysql --user=$DB_USER --password=$DB_PASSWD -e "$OrderDetailsInsert"
+
+exit 0
